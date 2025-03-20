94339e8b411b435af74331e62c77aaea999dea5d
Rodrigo Hurtado Costa <rodrigo.costa@skymsen.local>
2025-03-20 10:54:41 -0300

    Página inicial da aplicação

---
+++ views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventário de Celulares</title>
+</head>
+<body>
+    <h1>Inventário</h1>
+</body>
+</html>
