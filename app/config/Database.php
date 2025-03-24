289d16265cd56030a96a68d1e503b82c8e343db7
Rodrigo Hurtado Costa <rodrigo.costa@skymsen.local>
2025-03-24 16:23:25 -0300

    Arquivo de configuração do banco de dados

---
+++ app/config/Database.php
@@ -0,0 +1,21 @@
+<?php 
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'inventario-celulares');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+function getDBConnection()
+{
+    try {
+        // criando uma conexão
+        $pdo = new PDO("mysql:host=". DB_HOST.";dbname=".DB_NAME,  DB_USER, DB_PASS);
+        // definindo o modo de erro do PDO
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conectado ao banco de dados com sucesso";
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Falha na conexão " . $e->getMessage());
+    }
+
+}
