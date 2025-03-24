a220c94f75019d101d15fa1d4c07cfd469a38a96
Rodrigo Hurtado Costa <rodrigo.costa@skymsen.local>
2025-03-24 16:28:55 -0300

    View responsável por listar os celulares

---
+++ app/views/lista_celulares.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Inventário de Celulares</title>
+</head>
+<body>
+
+    <div class="container py-5">
+        <h1 class="text-center text-light mb-4 bg-dark">Inventário de Celulares</h1>
+        
+        <!-- Formulário de busca -->
+        <div class="mb-4">
+            <form id="search-form" method="GET" action="">
+            <input type="hidden"  id="userId">
+                <div class="input-group">
+                    <input type="text" class="form-control" id="idq" autocomplete="off" placeholder="Informe o ID/Usuário" required>
+                    <button class="btn btn-primary" type="submit">Buscar</button>
+                </div>
+            </form>
+        </div>
+
+</body>
+</html>
