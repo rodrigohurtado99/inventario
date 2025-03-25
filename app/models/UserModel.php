09bb7c7029b2c0e3dee391cd5c935a84d94b6bca
Rodrigo Hurtado Costa <rodrigo.costa@skymsen.local>
2025-03-25 16:40:04 -0300

    Responsável pelas requisições ao banco de dados

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -1,8 +1,21 @@
 <?php 
-
 // importa a conexão ao banco de dados
 require_once BASE_PATH . "/app/config/Database.php";
-class UserModel
+class User
 {
+    public $pdo;
     
+    public function __construct()
+    {
+        global $pdo;
+        $this->pdo = $pdo;
+    }
+    public function ListarCelulares()
+    {
+        
+        $sql = "SELECT * FROM celulares";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
